9a3d9c0042df6146fa7d36d2ea829dc5def6a0fb
Tarun Amasa <tarun.amasa@apple.com>
2022-07-11 18:10:16 -0700

    added documentation for version-patch

--- src/utils/version-patch.js
+++ src/utils/version-patch.js
@@ -12,18 +12,34 @@
 import { apply } from './json-patch';
 import { clone } from './data';
 
-function getDiffIndex(displayName, data) {
-  const index = data.versions.findIndex(item => item.version.displayName === displayName);
-  return index;
-}
-
+/**
+ * Checks if a JSON file (typically index.json or a RenderNode) has version data
+ * @param  {JSON} data to be inspected for version data
+ * @returns {Boolean}
+ */
 function dataHasVersion(data) {
   if (!data) return false;
   if (!('metadata' in data)) return false;
   if (!('version' in data.metadata)) return false;
   return true;
 }
-
+/**
+ * Find the index of a specific Display Name in the version list
+ * @param  {string} displayName
+ * @param  {JSON} data
+ * @returns {number} index of display name, -1 if not found.
+ */
+function getDiffIndex(displayName, data) {
+  if (!dataHasVersion(data)) return -1;
+  if (!('versions' in data)) return -1;
+  const index = data.versions.findIndex(item => item.version.displayName === displayName);
+  return index;
+}
+/**
+ * Initialize a list of versions for a given JSON file
+ * @param  {JSON} data
+ * @returns {Array} of versions
+ */
 function initializeVersionList(data) {
   if (dataHasVersion(data)) {
     let versions = [];
@@ -35,7 +51,13 @@ function initializeVersionList(data) {
   }
   return null;
 }
-
+/**
+ * Patch a given JSON file to a different version/state given a display name
+ * If displayName doesn't exist in the JSON file, return original data
+ * @param  {string} displayName
+ * @param  {JSON} data
+ * @return {JSON} patchedversioned data
+ */
 function patchToVersion(displayName, data) {
   if (!data) return null;
   if (!displayName) return data;
@@ -44,7 +66,6 @@ function patchToVersion(displayName, data) {
   if (patchIndex === -1) {
     return data;
   }
-
   let patchJSON = clone(data);
   for (let i = 0; i <= patchIndex; i++) {
     patchJSON = apply(patchJSON, data.versions[i].patch);
